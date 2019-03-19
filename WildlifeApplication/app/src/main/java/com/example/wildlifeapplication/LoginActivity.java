d1781eaf765fc554b24726741a1ed2a766c60b3c
Finlay Wallis <wallisf@cardiff.ac.uk>
2019-03-19 14:56:05 +0000

    created Login interface

---
+++ WildlifeApplication/app/src/main/java/com/example/wildlifeapplication/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.wildlifeapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText mTextUsername;
+    EditText mTextPassword;
+    Button mButtonLogin;
+    TextView mTextViewRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mTextUsername = (EditText) findViewById(R.id.editText_username);
+        mTextPassword = (EditText) findViewById(R.id.editText_password);
+        mButtonLogin = (Button) findViewById(R.id.button_login);
+        mTextViewRegister = (TextView) findViewById(R.id.textView_register);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+    }
+}
