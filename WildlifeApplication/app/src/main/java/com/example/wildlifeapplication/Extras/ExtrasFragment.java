61a685808b492b0c9341c70340a3e853d88b34e6
Joe Baxendale <BaxendaleJ@cardiff.ac.uk>
2019-03-23 14:43:56 +0000

    added listview to the extras fragment and added some options. no functionality.

--- WildlifeApplication/app/src/main/java/com/example/wildlifeapplication/Extras/ExtrasFragment.java
+++ WildlifeApplication/app/src/main/java/com/example/wildlifeapplication/Extras/ExtrasFragment.java
@@ -2,46 +2,35 @@ package com.example.wildlifeapplication.Extras;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.wildlifeapplication.R;
 
+import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ExtrasFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ExtrasFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ExtrasFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+public class ExtrasFragment extends ListFragment {
+
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+    private ArrayList<String> Extras_List = new ArrayList<>();
+    private ArrayAdapter<String> mAdapter;
+    ListView listView;
 
     public ExtrasFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ExtrasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ExtrasFragment newInstance(String param1, String param2) {
         ExtrasFragment fragment = new ExtrasFragment();
         Bundle args = new Bundle();
@@ -54,6 +43,7 @@ public class ExtrasFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -61,10 +51,13 @@ public class ExtrasFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_extras, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_extras, container,false);
+        listView = v.findViewById(android.R.id.list);
+        getOptions();
+        mAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1,Extras_List );
+        listView.setAdapter(mAdapter);
+        return v;
     }
 
 
@@ -82,5 +75,11 @@ public class ExtrasFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    public void getOptions(){
+        Extras_List.add("Settings");
+        Extras_List.add("Statistics");
+        Extras_List.add("Log Out");
     }
 }
